b91eb91e4aa91d5ee79413ec385b849068877e80
Garry Rodriguez - Kredo <garry@Garrys-MacBook-Air.local>
2025-04-30 14:17:30 +0800

    change the page entirely

--- main.php
+++ main.php
@@ -3,11 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Main</title>
+    <title>Test Change</title>
 </head>
 <body>
-    <h1>Hello, from GI Class</h1>
-    <h1>hello again</h1>
-    <h1>hello from shin</h1>
+    <h1>Hello changes!</h1>
 </body>
 </html>
