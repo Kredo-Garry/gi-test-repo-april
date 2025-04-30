e19b73e24a798224c42877530266b17e7384892d
nsk0201 <2blzxt02j@gmail.com>
2025-04-30 14:44:23 +0800

    change

--- main.php
+++ main.php
@@ -6,11 +6,6 @@
     <title>Test Change</title>
 </head>
 <body>
-<<<<<<< HEAD
-    <h1>Hello changes2!</h1>
-=======
-    <h1>Hello changes!</h1>
-    <h1>Change by ken</h1>
->>>>>>> 9e1c08f1250345afa74b8bfc9eaf1cde7765a281
+    <h1>hello from shin</h1>
 </body>
 </html>
